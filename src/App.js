5064a05b8bbc4612712dd2399f7a071c527a1127
nzaytsev <nikolai.zaytsev@gitkraken.com>
2024-09-10 10:59:35 +0700

    ST-171 - commit for tag 1

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         {/* commit for tag */}
+        {/* commit for tag1 */}
         <a
           className="App-link"
           href="https://reactjs.org"

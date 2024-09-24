1a8c293d423391d92b2868ea8066a8fcce6113c7
nzaytsev <nikolai.zaytsev@gitkraken.com>
2024-09-24 09:07:45 +0700

    5

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           {/* <p> */}
           {/* <p> */}
+          {/* <p> */}
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a

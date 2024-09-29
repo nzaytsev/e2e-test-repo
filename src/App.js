07d58e6f353de014bd6a63b4ee0eab0498fddd3f
nzaytsev <nikolai.zaytsev@gitkraken.com>
2024-09-29 07:20:28 +0700

    test gh-NUM123 key

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         {/* 1 */}
         {/* 2 */}
+        {/* 3 */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
